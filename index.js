97b8199f0aca254c8f844adf733c858ce5bd722e
Alpha <alpha.matara@student.moringaschool.com>
2024-03-19 14:56:38 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 1000 ) + 1;
+const num3 = 57;
+const num4 = 53;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
